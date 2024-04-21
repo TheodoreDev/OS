ebcc25d950a701f0ae6fc9dc69e5c232c81cf661
TheodoreDev <theodore.delbove@gmail.com>
2024-04-21 18:52:53 +0200

    - Make interface more like windows or Linux cmd - Add own file for print file table - Add clear command (cls)

--- src/asm/kernel.asm
+++ src/asm/kernel.asm
@@ -36,7 +36,7 @@ command_loop:
 
 run_command:
 	cmp cx, 0
-	je input_not_found			; handle empty input
+	je get_input				; handle empty input
 	
 	mov byte [si], 0			; null terminate cmdString from di
 	mov si, cmdString			; reset di to point to start of user input
@@ -45,7 +45,7 @@ check_commands:
 	push cx
 	mov di, cmdDir				; file table command
 	repe cmpsb
-	je file_browser
+	je fileTable
 	
 	pop cx						; restore cx from the stack
 	push cx
@@ -76,121 +76,27 @@ check_commands:
 	je end_program
 	
 	pop cx						; restore cx from the stack
+	push cx
+	mov di, cmdClear			; clear the screen by scrolling
+	mov si, cmdString			; reset di to point to start of user input
+	repe cmpsb
+	je clear_screen
+
+	pop cx						; restore cx from the stack
+	
 
 check_files:
-
-input_not_found:
-	mov si, failMsg				; command not found
-	call print_string
-	jmp get_input
-
-;; menu F) File Browser
-file_browser:
-	;; Set window and clear it
-	call resetTextScreen
-
-	mov si, fileTableHeading
-	call print_string
-	
-	;; load file table string
-	xor cx, cx					; reset counter for # of byte at current file table entry
-	mov ax, 0x1000				; file table loc
-	mov es, ax					; ES = 0x1000
-	xor bx, bx					; ES:BX = 0x1000:0
-	mov ah, 0x0e				; get ready to print screen
-	
-	mov al, [ES:BX]
-
-fileName_loop:
-	mov al, [ES:BX]
-	cmp al, 0					; at end of file tab ? or file name null ?
-	je get_program_name			; no more names? at the end of file table ?
-
-	int 0x10					; print char in al to screen
-	cmp cx, 9 					; at the end of name ?
-	je file_ext
-	inc cx						; increment file entry byte counter
-	inc bx						; get next byte at file table
-	jmp fileName_loop
-
-file_ext:
-	mov cx, 3
-	call print_blanks_loop
-
-	inc bx
-	mov al, [ES:BX]
-	int 0x10
-	inc bx
-	mov al, [ES:BX]
-	int 0x10
-	inc bx
-	mov al, [ES:BX]
-	int 0x10
-
-dir_entry_number:
-	mov cx, 9
-	call print_blanks_loop
-	
-	inc bx
-	mov al, [ES:BX]
-	call print_hex_as_ascii
-
-start_sector_number:
-	mov cx, 9
-	call print_blanks_loop
-	
-	inc bx
-	mov al, [ES:BX]
-	call print_hex_as_ascii
-
-file_size:
-	mov cx, 14
-	call print_blanks_loop
-	
-	inc bx
-	mov al, [ES:BX]
-	call print_hex_as_ascii
-	mov al, 0xA
-	int 0x10
-	mov al, 0xD
-	int 0x10
-
-	inc bx						; get first byte of next file name
-	xor cx, cx					; reset counter for next file name
-	jmp fileName_loop
-
-get_program_name:
-	mov al, 0xA
-	int 0x10
-	mov al, 0xD
-	int 0x10
-	mov di, cmdString			; di now pointing to cmdString
-	mov byte [cmdLength], 0		; reset counter
-
-program_name_loop:
-	mov ax, 0x00
-	int 0x16					; BIOS int get keystroke
-
-	mov ah, 0x0e
-	cmp al, 0xD					; did user press 'enter' key?
-	je start_search
-
-	inc byte [cmdLength]		; add to counter
-	mov [di], al				; store input char to command string
-	inc di						; go to next byte at di
-	int 0x10					; print input char to screen
-	jmp program_name_loop		; loop for next char from user
-
-start_search:
-	mov di, cmdString			; reset di, points to command string
+	mov ax, 0x1000
+	mov es, ax					; reset ES:BX to beginning of the file table
 	xor bx, bx					; reset ES:BX to point to beginning of file table
+	mov si, cmdString			; reset si to start of user input
 
 check_next_char_pgm:
 	mov al, [ES:BX]				; get file table char
 	cmp al, 0 					; at the end of the file table ?
-	je pgm_not_found			; if yes, pgm not found
+	je input_not_found			; if yes, pgm not found
 
-	cmp al, [di]				; does user input match file table char ?
+	cmp al, [si]				; does user input match file table char ?
 	je start_compare_pgm
 
 	add bx, 16					; get next char in file table
@@ -198,35 +104,23 @@ check_next_char_pgm:
 
 start_compare_pgm:
 	push bx						; save file table position
-	mov byte cl, [cmdLength]
 
 compare_pgm_loop:
 	mov al, [ES:BX]				; get file table char
 	inc bx						; next byte in input/filetable
-	cmp al, [di]				; does input match filetable char
+	cmp al, [si]				; does input match filetable char
 	jne restart_search			; if not, search again from here
 
 	dec cl						; decrement length counter
 	jz found_pgm				; counter = 0, program found
-	inc di						; go to next byte of input
+	inc si						; go to next byte of input
 	jmp compare_pgm_loop
 
 restart_search:
-	mov di, cmdString			; reset to start of user input
+	mov si, cmdString			; reset to start of user input
 	pop bx						; restore file table position
 	inc bx						; go to next char in file table
 	jmp check_next_char_pgm		; start checking again
-
-pgm_not_found:
-	mov si, pgmNotFoundString	; program not found
-	call print_string
-	mov ah, 0x00				; get keystroke
-	int 0x16
-	mov ah, 0x0e
-	int 0x10
-	cmp al, "Y"
-	je file_browser				; reload file browser
-	jmp fileTable_end			; go back to main menu
 
 ;; Read disk sector of pgm to memory and execute it by far jmp
 found_pgm:
@@ -254,9 +148,7 @@ found_pgm:
 
 	mov si, pgmNotLoaded		; pgm not loaded correctly, error
 	call print_string
-	mov ah, 0x00				; get keystroke
-	int 0x16
-	jmp file_browser			; go back to file browser
+	jmp get_input				; go back to prompt
 
 pgm_loaded:
 	mov ax, 0x8000				; pgm loaded, set segment reg to location
@@ -267,33 +159,29 @@ pgm_loaded:
 	mov ss, ax
 	jmp 0x8000:0x0000			; far jmp to pgm
 
-fileTable_end:
-	mov si, goBackMsg			; show go back msg
+input_not_found:
+	mov si, failMsg				; command not found
 	call print_string
-	mov ah, 0x00				; get keystroke
-	int 0x16
-	jmp main_menu				; go back to main menu
+	jmp get_input
 
-;; menu R) Reboot
+;; Command File Table
+fileTable:
+	call print_fileTable
+	jmp get_input
+
+;; Command Reboot
 reboot:
 	jmp 0xFFFF:0x0000
 
-;; menu P) Print register values
+;; Command Print register values
 print_registers_values:
-	;; Set window and clear it
-	call resetTextScreen
-
 	mov si, printRegHeading
 	call print_string
 
 	call print_registers
+	jmp get_input
 
-	mov si, goBackMsg			; show go back message
-	call print_string
-	mov ah, 0x00				; get key stroke
-	int 0x16
-	jmp main_menu				; go back to main menu
-
+;; Command graphics mode test
 graphics_test:
 	;; Set window and clear it (gfx mode)
 	call resetGraphicsScreen
@@ -325,6 +213,11 @@ squareColLoop:
 	int 0x16
 	jmp main_menu				; go back to main menu
 
+;; Command clear screen
+clear_screen:
+	call resetTextScreen
+	jmp get_input
+
 ;; menu N) End program
 end_program:
 	cli							; clear interrupts
@@ -344,55 +237,54 @@ hexNum:
 
 ;; print out cx number of space to screen
 print_blanks_loop:
-	cmp cx, 0
-	je end_blanks_loop
 	mov ah, 0x0e
 	mov al, " "
 	int 0x10
-	dec cx
-	jmp print_blanks_loop
-
-end_blanks_loop:
+	loop print_blanks_loop
 	ret
 
 ;; Include other file(s)
 include "../print/print_string.asm"
 include "../print/print_hex.asm"
 include "../print/print_registers.asm"
+include "../print/print_fileTable.asm"
+;;include "../screen/clear_screen.asm"
 include "../screen/set_text_screen.asm"
 include "../screen/set_graphics_screen.asm"
 
+nl equ 0xA, 0xD
+
 startMessage:
-        db "--------------------------------", 0xA, 0xD,\
-	"Kernel Booted, Welcome to TedOS.", 0xA, 0xD,\
-	"--------------------------------", 0xA, 0xD, 0xA, 0xD, 0
+        db "--------------------------------", nl,\
+	"Kernel Booted, Welcome to TedOS.", nl,\
+	"--------------------------------", nl, 0
 prompt:
-	db ">", 0
+	db nl, ">", 0
 	
 success:
-	db 0xA, 0xD, "Command/Program found !", 0xA, 0xD, 0
+	db nl, "Command/Program found !", nl, 0
 failMsg:
-	db 0xA, 0xD, "Command or program not found :(", 0xA, 0xD, 0
+	db nl, "Command/Program not found :(", nl, 0
 pgmNotFoundString:
-	db 0xA, 0xD, "Program not found, try again ? (Y)", 0xA, 0xD, 0
+	db nl, "Program/file not found, try again ? (Y)", nl, 0
 sectorNotFound:
-	db 0xA, 0xD, "Sector not found, try again ? (Y)", 0xA, 0xD, 0
+	db nl, "Sector not found, try again ? (Y)", nl, 0
 pgmNotLoaded:
-	db 0xA, 0xD, "Program not loaded correctly !",\
-	0xA, 0xD, "Press any key to go back to file browser ...", 0xA, 0xD, 0
-goBackMsg:mov al, 0xA
-	db 0xA, 0xD, 0xA, 0xD, "Press any key to go back ...", 0
+	db nl, "Program found but not loaded correctly !", nl, 0
+goBackMsg:
+	db nl, nl, "Press any key to go back ...", 0
 dbgTest:
 	db "Test", 0
 
-fileTableHeading:   db "----------   ---------   -------   ------------   --------------",\
-	0xA,0xD,"File Name    Extension   Entry #   Start Sector   Size (sectors)",\
-	0xA,0xD,"----------   ---------   -------   ------------   --------------",\
-	0xA,0xD,0
+fileTableHeading:
+	db nl, "----------   ---------   -------   ------------   --------------",\
+	nl,"File Name    Extension   Entry #   Start Sector   Size (sectors)",\
+	nl,"----------   ---------   -------   ------------   --------------",\
+	nl,0
 printRegHeading:
-	db "--------  ------------", 0xA, 0xD,\
-	"Register  Mem Location", 0xA, 0xD,\
-	"--------  ------------", 0xA, 0xD, 0
+	db nl, "--------  ------------", nl,\
+	"Register  Mem Location", nl,\
+	"--------  ------------", nl, 0
 
 cmdDir:
 	db "dir",0
@@ -404,6 +296,8 @@ cmdGfx:
 	db "gfx",0
 cmdHalt:
 	db "hlt",0
+cmdClear:
+	db "cls", 0
 
 cmdLength:
 	db 0
--- src/print/print_registers.asm
+++ src/print/print_registers.asm
@@ -2,49 +2,57 @@
 ;;;
 
 print_registers:
+	pusha
 	mov si, regString
 	call print_string
-	call print_hex			; print dx
+	call print_hex					; print dx
 
 	mov byte [regString+2], "a"
 	call print_string
 	mov dx, ax
-	call print_hex			; print ax
+	call print_hex					; print ax
 
 	mov byte [regString+2], "b"
-        call print_string
-        mov dx, bx
-        call print_hex                  ; print bx
+	call print_string
+	mov dx, bx
+	call print_hex                  ; print bx
 
 	mov byte [regString+2], "c"
-        call print_string
-        mov dx, cx
-        call print_hex                  ; print cx
+	call print_string
+	mov dx, cx
+	call print_hex                  ; print cx
 
 	mov word [regString+2], "si"
-        call print_string
-        mov dx, si
-        call print_hex                  ; print si
+	call print_string
+	mov dx, si
+	call print_hex                  ; print si
 
 	mov byte [regString+2], "d"
-        call print_string
-        mov dx, di
-        call print_hex                  ; print di
+	call print_string
+	mov dx, di
+	call print_hex                  ; print di
 
 	mov word [regString+2], "cs"
-        call print_string
-        mov dx, cs
-        call print_hex                  ; print cs
+	call print_string
+	mov dx, cs
+	call print_hex                  ; print cs
 
 	mov byte [regString+2], "d"
-        call print_string
-        mov dx, ds
-        call print_hex                  ; print ds
+	call print_string
+	mov dx, ds
+	call print_hex                  ; print ds
 
 	mov byte [regString+2], "e"
-        call print_string
-        mov dx, es
-        call print_hex                  ; print es
+	call print_string
+	mov dx, es
+	call print_hex                  ; print es
+
+	mov ah, 0x0e
+	mov al, 0xA
+	int 0x10
+	mov al, 0xD
+	int 0x10
+	popa
 
 	ret
 
